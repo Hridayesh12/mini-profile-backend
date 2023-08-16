e6751b6c54c4302c396d703b4eba602a08b93341
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-08-16 20:25:24 +0530

    Consoling

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const user_data = async (req, res) => {
     try {
         console.log(req.cookies);
-        let auth_token = req.cookies.jwt.jwt;
+        let auth_token = req.cookies?.jwt?.jwt;
         const token = auth_token;
         const response = jwt.verify(token, process.env.SECRET_KEY);
         res.json(response);
@@ -29,7 +29,6 @@ const login_post = asyncHandler(async (req, res) => {
         const user = await User.login(req.body);
         console.log(user._id);
         const token = createToken(user._id);
-        console.log(token);
         res.cookie('jwt', { jwt: token, httpOnly: true, maxAge: process.env.TOKEN_VALIDITY });
         res.status(200).json({ user: user._id });
     }
