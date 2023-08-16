78a0d383ab294680481083d6960087f77ad43746
HRIDAYESH PADALKAR <hridayeshpadalkar7@gmail.com>
2023-08-16 19:53:24 +0530

    Url changed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const connectDb = async () => {
 const app = express();
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://mini-profile.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
     }),
